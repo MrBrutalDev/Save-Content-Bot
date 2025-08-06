d3a85b934246aafb931d261d17b26d4bc44e8150
Mister Brutal <104305861+MrBrutalDev@users.noreply.github.com>
2025-08-06 22:27:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def welcome():
     # Render the welcome page with animated "Team SPY" text
-    return 'Hello, From Mr. Brutal'
+    return render_template("welcome.html")
 
 if __name__ == "__main__":
     # Default to port 5000 if PORT is not set in the environment
